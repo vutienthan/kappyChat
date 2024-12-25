4fda666877cd3638cdfa41210d444f091ad809ad
vutienthan <128622664+vutienthan@users.noreply.github.com>
2024-12-25 13:42:56 +0300

    Update Ppp.py

--- Ppp.py
+++ Ppp.py
@@ -1,16 +1,10 @@
-# Определяем логическую функцию
-def logical_function(w, x, y, z):
-    return ((y <= x) and (z or w)) <= (((x and not w) or (y == z)))
-
-# Печатаем заголовок
 print("w x y z F")
-
-# Генерируем таблицу истинности
+# Проходим по всем комбинациям значений переменных
 for w in range(2):
     for x in range(2):
         for y in range(2):
             for z in range(2):
-                # Вычисляем значение логической функции
-                F = int(logical_function(w, x, y, z))
-                # Печатаем строки таблицы истинности
-                print(w, x, y, z, F)
+                # Логическое выражение
+                F = ((y <= x) and (z or w)) <= ((x and not w) or (y == z))
+                # Вывод комбинаций переменных и значения функции
+                print(w, x, y, z, int(F))
